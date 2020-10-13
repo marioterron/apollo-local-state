0af130fbb89bca699db1e9a6ceb34872d971f452
Mario Terron mario.terron@beezy.net <mario.terron@beezy.net>
2020-10-14 00:44:03 +0200

    Add the key property to List map

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const List = ({ todos }) => <ul>{!!todos.length && todos.map((t) => <li>{t}</li>)}</ul>;
+const List = ({ todos }) => (
+  <ul>{!!todos.length && todos.map((todo, index) => <li key={new Date().getTime().toString() + index}>{todo}</li>)}</ul>
+);
 
 export default List;
