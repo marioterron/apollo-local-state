6bb83db269438a05563676e00e810326d42529e4
Mario Terron mario.terron@beezy.net <mario.terron@beezy.net>
2020-11-08 12:43:32 +0100

    feat: add to delete action to list container

--- src/container/List.js
+++ src/container/List.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { update } from '../store/actions';
+import { remove, update } from '../store/actions';
 import List from '../components/List';
 
 const ListContainer = ({ state, dispatch }) => {
+  const handleUseReducerRemove = (value, id) => {
+    dispatch(remove({ id, value }));
+  };
+
   const handleUseReducerUpdate = (value, id) => {
     dispatch(update({ id, value }));
   };
 
-  return <List todos={state} onUpdateItem={handleUseReducerUpdate} />;
+  return <List todos={state} onDeleteItem={handleUseReducerRemove} onUpdateItem={handleUseReducerUpdate} />;
 };
 
 export default ListContainer;
