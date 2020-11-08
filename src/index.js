4943a7f165549582fef0a7bedc7a7b54b3bf9e5d
Mario Terron mario.terron@beezy.net <mario.terron@beezy.net>
2020-11-08 13:26:44 +0100

    refactor: move home logic component to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import Input from './container/Input';
+import List from './container/List';
+import { initialState, reducer } from './store/reducer';
 import './index.css';
+
+const Index = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <Input dispatch={dispatch} />
+      <List state={state} dispatch={dispatch} />
+    </>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
