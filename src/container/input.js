6afb4a06a52aaa4c3402ca641c99c202ad99147f
Mario Terron mario.terron@beezy.net <mario.terron@beezy.net>
2020-11-08 13:43:21 +0100

    fix: correct casing problems in repo

--- src/container/input.js
+++ src/container/input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Input from '../components/Input';
+import Input from '../components/input';
 import { add } from '../store/actions';
 
 const InputContainer = ({ dispatch }) => {
